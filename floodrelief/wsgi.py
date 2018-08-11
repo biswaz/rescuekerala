55c69a751256a228adae641c0414db00b2904cbf
Biswas B <Biswasb007@gmail.com>
2018-08-11 19:42:23 +0530

    fixed whitenoise

--- floodrelief/wsgi.py
+++ floodrelief/wsgi.py
@@ -15,4 +15,3 @@ from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'floodrelief.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
