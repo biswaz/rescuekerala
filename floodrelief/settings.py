dab8113339eac4e3ccc20efc17680cfc3ea744ed
Shameel Kadannamanna <shameelsadaka@gmail.com>
2018-08-12 00:19:24 +0530

    IEEE logo added

--- floodrelief/settings.py
+++ floodrelief/settings.py
@@ -74,12 +74,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'floodrelief.wsgi.application'
 
-DATABASES = {}
+DATABASES = {
+    'default':{
+        'ENGINE':'django.db.backends.sqlite3',
+        'NAME':os.path.join(BASE_DIR,'db.sqlite3')
+    }
+}
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config(
-	default='postgres://kruser:password@localhost/krdb')
+# DATABASES['default'] = dj_database_url.config(
+# 	default='postgres://kruser:password@localhost/krdb')
 
 
 # Password validation
