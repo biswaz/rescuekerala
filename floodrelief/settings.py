03967b27be84a5ef12746d4a078cf556168171b7
Biswas B <Biswasb007@gmail.com>
2018-08-11 21:49:04 +0530

    added styles

--- floodrelief/settings.py
+++ floodrelief/settings.py
@@ -121,6 +121,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# STATICFILES_DIRS = (
-# 	os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+	os.path.join(BASE_DIR, 'static'),
+)
