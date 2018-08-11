81abc9c45826dde756f3cd46b95cc34c2d442a20
Biswas B <Biswasb007@gmail.com>
2018-08-11 19:21:54 +0530

    Added postgres

--- floodrelief/settings.py
+++ floodrelief/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -72,16 +73,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'floodrelief.wsgi.application'
 
-
+DATABASES = {}
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES['default'] = dj_database_url.config(
+	default='postgres://kruser:password@localhost/krdb')
 
 
 # Password validation
