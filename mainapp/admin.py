0e023912cf59858c2bd30f1d158ece4667665883
Biswas B <Biswasb007@gmail.com>
2018-08-15 14:16:30 +0530

    admin can sort volunteers by district

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 from .models import Request, Volunteer, Contributor, DistrictNeed, DistrictCollection, DistrictManager
 
+
 class RequestAdmin(admin.ModelAdmin):
     readonly_fields = ('dateadded',)
     ordering = ('district',)
 
+
+class VolunteerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'organisation',)
+    list_filter = ('district', )
+
 admin.site.register(Request, RequestAdmin)
-admin.site.register(Volunteer)
+admin.site.register(Volunteer, VolunteerAdmin)
 admin.site.register(Contributor)
 admin.site.register(DistrictNeed)
 admin.site.register(DistrictCollection)
