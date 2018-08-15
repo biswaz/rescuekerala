98f3ab4f028df13595b6fa8712f431bab157d37d
Biswas B <Biswasb007@gmail.com>
2018-08-15 14:56:56 +0530

    changed admin panel header

--- floodrelief/urls.py
+++ floodrelief/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+
+admin.site.site_header = settings.ADMIN_SITE_HEADER
 
 urlpatterns = [
     path('', include('mainapp.urls')),
